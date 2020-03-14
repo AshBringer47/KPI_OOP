6bce6a51192c9be175b6cf18c2bf63ab16c64b64
AshBringer47 <shiron.kaz@gmail.com>
2020-03-15 00:52:49 +0200

    Added lab. work 3 in C#

---
+++ Lab3/Lab3_CS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab3_CS
+{
+    class Program
+    {
+        private static void Main()
+        {
+            MyClass arr = new MyClass(5);
+            Console.WriteLine(arr.LogicalSum);
+        }
+    }
+}
---
+++ Lab3/Lab3_CS/myClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3_CS
+{
+    class MyClass
+    {
+        private bool[] myArray;
+        private bool logicalSum;
+        public MyClass(int size)
+        {
+            Array.Resize(ref myArray, size);
+        }
+        public bool LogicalSum
+        {
+            get
+            {
+                bool result = myArray[0];
+                for (int i = 1; i < myArray.Length; i++)
+                    result |= myArray[i];
+                logicalSum = result;
+                return logicalSum;
+            }
+        }
+        public bool this[int index]
+        {
+            get
+            {
+                try
+                {
+                    return (Convert.ToBoolean(Convert.ToInt32(myArray[index]) ^ 1)) ;
+                }
+                catch(Exception e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+            set
+            {
+                try
+                {
+                    myArray[index] = value;
+                }
+                catch(Exception e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+        }
+
+    }
+}
