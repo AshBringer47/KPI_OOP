46927344f8d2bdd70a3f97776bba277924207a91
AshBringer47 <shiron.kaz@gmail.com>
2020-03-15 02:30:51 +0200

    Added lab. work 4 in C#

---
+++ Lab4/Lab4_CS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab4_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass S1 = new myClass(); // via default constructor.
+            myClass S2 = new myClass("Hello #Worl##d#"); // via parameter.
+            myClass S3 = new myClass(S2); // via copying.
+            Console.WriteLine(S1.Value);
+            Console.WriteLine(S2.Value);
+            Console.WriteLine(S3.Value);
+            S2 -= '#';
+            Console.WriteLine(S2.Value);
+            S1 = S2 + S3;
+            Console.WriteLine(S1.Value);
+
+        }
+    }
+}
---
+++ Lab4/Lab4_CS/myClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4_CS
+{
+    class myClass
+    {
+        private char[] _myRow;
+        public myClass() { }
+        public myClass(string newString)
+        {
+            _myRow = newString.ToCharArray();
+        }
+        public myClass(myClass previousString)
+        {
+            _myRow = previousString.Value;
+        }
+        public char[] Value
+        {
+            get => _myRow;
+            set => _myRow = value;
+        }
+        public int Length()
+        {
+            return _myRow.Length;
+        }
+        public static myClass operator +(myClass string1, myClass string2)
+        {
+            string temp = "";
+            foreach (char symbol in string1.Value)
+            {
+                temp += symbol;
+            }
+            foreach (char symbol in string2.Value)
+            {
+                temp += symbol;
+            }
+            myClass result = new myClass
+            {
+                Value = temp.ToCharArray() /*(string1.Value.ToString() + string2.Value.ToString()).ToCharArray()*/
+            };
+            return result;
+        }
+        public static myClass operator -(myClass string1, char newSymbol)
+        {
+            string temp = "";
+            foreach (char symbol in string1.Value)
+            {
+                if (symbol == newSymbol)
+                    continue;
+                temp += symbol;
+            }
+            myClass result = new myClass
+            {
+                Value = temp.ToCharArray()
+            };
+            return result;
+        }
+    }
+}
